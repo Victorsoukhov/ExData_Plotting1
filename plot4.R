3cc8a40d8cfa34a291e80e6b886a42e2fa4f37a0
Victorsoukhov <Victorsoukhov@gmail.com>
2015-07-12 16:41:47 +0300

    Repo update

--- plot4.R
+++ plot4.R
@@ -17,11 +17,14 @@ plot4 <- function(){
     #plot4
     png("plot4.png", width = 480, height = 480)
     
+    #plot columnwise, with borders
     par(mfcol = c(2, 2), mar = c(4, 4, 1, 2), oma = c(1, 0, 0, 0))
     
+    #plot1
     plot(x<-td$DateTime, y<- td$Global_active_power, type = "l", 
          xlab = "", ylab = "Global Active Power")
     
+    #plot2
     plot(x<-td$DateTime, y<- td$Sub_metering_1, type = "l", 
          xlab = "", ylab = "Energy sub metering")
     points(x<-td$DateTime, y<- td$Sub_metering_2, type = "l", col="red")
@@ -29,9 +32,11 @@ plot4 <- function(){
     legend("topright", legend=c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"), 
            col = c("black", "red", "blue"), lty = 1, bty = "n")
     
+    #plot3
     plot(x<-td$DateTime, y<- td$Voltage, type = "l", 
          xlab = "datetime", ylab = "Voltage")
     
+    #plot4
     plot(x<-td$DateTime, y<- td$Global_reactive_power, type = "l", 
          xlab = "datetime", ylab = "Global_reactive_power")
     
